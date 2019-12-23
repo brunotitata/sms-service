a93b42a0d3a3ee5bf047503716fb1aece302adba
Bruno <brunotitata@gmail.com>
2019-12-22 22:41:19 -0300

    adicionando novas api

--- src/main/java/br/com/sms/controller/SmsController.java
+++ src/main/java/br/com/sms/controller/SmsController.java
@@ -4,6 +4,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.sms.DTO.SmsDTO;
@@ -11,6 +12,7 @@ import br.com.sms.model.SMS;
 import br.com.sms.service.SmsService;
 
 @RestController
+@RequestMapping("/api")
 public class SmsController {
 
     private SmsService smsService;
@@ -19,7 +21,7 @@ public class SmsController {
 	this.smsService = smsService;
     }
 
-    @PostMapping("/api/send")
+    @PostMapping("/send")
     @PreAuthorize("hasRole('USER')")
     public ResponseEntity<SMS> sendSMS(@RequestBody SmsDTO sms) {
 
