75efe868d8b0cf2b1b4e1e1dbbb69e5fc5a51902
Bruno Costa <bruno.costa@idtrust.com.br>
2020-02-07 16:18:35 -0300

    thread sleep

--- src/main/java/br/com/sms/SmsServiceApplication.java
+++ src/main/java/br/com/sms/SmsServiceApplication.java
@@ -1,13 +1,35 @@
 package br.com.sms;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmsServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmsServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+	Thread thread = new Thread(new Runnable() {
+
+	    @Override
+	    public void run() {
+		while (true) {
+		    try {
+			Thread.sleep(600000L);
+		    } catch (InterruptedException e) {
+			e.printStackTrace();
+		    }
+
+		    System.out.println("Verificado: " + LocalDateTime.now());
+
+		}
+
+	    }
+	});
+	thread.start();
+
+	SpringApplication.run(SmsServiceApplication.class, args);
+    }
 
 }
